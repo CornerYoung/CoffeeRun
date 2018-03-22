1a6910bdc67888aa30ff950c688b284d9f1feb48
ygn <947498533@qq.com>
2018-03-22 15:20:35 +0800

    添加提交处理程序（*****），提取数据

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -4,6 +4,7 @@
   var $ = window.jQuery;
 
   function FormHandler(selector){
+
     if(!selector){
       throw new Error('No selector provided');//未传入selector则抛出异常
     }
@@ -12,6 +13,17 @@
     if(this.$formElement.length === 0){
       throw new Error('Could not find element with selector: '+selector);
     };
+
+  };
+
+  FormHandler.prototype.addSubmitHandler = function(){
+    console.log('Setting submit handler for form');
+    this.$formElement.on('submit',function(event){
+      event.preventDefault();
+
+      var data = $(this).serializeArray();
+      console.log(data);
+    });
   };
 
   App.FormHandler = FormHandler;
