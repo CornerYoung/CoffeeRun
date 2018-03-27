68756ea1d6402f0f5b5ce40fd4352f7dcf200c21
ygn <947498533@qq.com>
2018-03-27 13:44:24 +0800

    从服务器检索数据

--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -11,9 +11,23 @@
     this.serverUrl = url;
   };
 
-  RemoteDataStore.prototype.add = function(key,val){
+  RemoteDataStore.prototype.add = function(key,val){  //将独立订单存储到服务器
     $.post(this.serverUrl,val,function(serverResponse){
       console.log(serverResponse);
+    });
+  };
+
+  RemoteDataStore.prototype.getAll = function(cb){  //从服务器读取所有订单
+    $.get(this.serverUrl,function(serverResponse){
+      console.log(serverResponse);
+      cb(serverResponse);
+    })
+  };
+
+  RemoteDataStore.prototype.get = function(key,cb){
+    $.get(this.serverUrl + '/' +key,function(serverResponse){
+      console.log(serverResponse);
+      cb(serverResponse);
     });
   };
 
